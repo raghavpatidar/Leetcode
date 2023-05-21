e3eca019e57210f8eb2daee0c645236ef0803895
raghavpatidar <raghavpatidar997764@gmail.com>
2023-05-21 19:15:12 +0530

    course-schedule-2

---
+++ DSA/Graph/topological sort/course-schedule-2.cpp
@@ -0,0 +1,39 @@
+//--------- Raghav  Patidar---------
+// Fuck Ratings,Just Enjoy the Contest/Questions 
+// I'm in Love with Experience and Learning.
+// Prob :  https://leetcode.com/problems/course-schedule-ii/description/
+// Day : 
+
+class Solution {
+public:
+    vector<int> topo(int n, vector<vector<int>>& pre) {
+        queue<int> q;
+        vector<int> in(n);
+        vector<int> adj[n];
+        vector<int> ans;
+        for (auto i : pre) {
+            adj[i[0]].push_back(i[1]);
+            in[i[1]]++;
+        }
+        for (int i = 0; i < n; i++) {
+            if (in[i] == 0)q.push(i);
+        }
+        while (q.size() > 0) {
+            int curr = q.front();
+            q.pop();
+            ans.push_back(curr);
+            for (int i : adj[curr]) {
+                in[i]--;
+                if (in[i] == 0) q.push(i);
+            }
+        }
+        if (ans.size() == n) {
+            reverse(ans.begin(), ans.end());
+            return ans;
+        }
+        return {};
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        return  topo(numCourses, prerequisites);
+    }
+};
