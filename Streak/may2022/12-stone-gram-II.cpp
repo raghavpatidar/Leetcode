23c7816e2ceca47e9f286bf2a6360c912d652e82
raghavpatidar <raghavpatidar997764@gmail.com>
2023-05-26 12:00:45 +0530

    day-384

---
+++ Streak/may2022/12-stone-gram-II.cpp
@@ -0,0 +1,29 @@
+//--------- Raghav  Patidar---------
+// Fuck Ratings,Just Enjoy the Contest/Questions 
+// I'm in Love with Experience and Learning.
+// Prob : https://leetcode.com/problems/stone-game-ii/
+// Day : 384
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int length = piles.size();
+        vector<vector<int>>dp(length + 1, vector<int>(length + 1, 0));
+        vector<int> sufsum(length + 1, 0);
+        for (int i = length - 1; i >= 0; i--) {
+            sufsum[i] = sufsum[i + 1] + piles[i];
+        }
+        for (int i = 0; i <= length; i++) {
+            dp[i][length] = sufsum[i];
+        }
+        for (int i = length - 1; i >= 0; i--) {
+            for (int j = length - 1; j >= 1; j--) {
+                for (int X = 1; X <= 2 * j && i + X <= length; X++) {
+                    dp[i][j] = max(dp[i][j], sufsum[i] - dp[i + X][max(j, X)]);
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
