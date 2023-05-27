f177f48d9cf05a4b6faf8f667aba9aef1f5a7365
raghavpatidar <raghavpatidar997764@gmail.com>
2023-05-27 20:53:29 +0530

    queue using stack amortized

---
+++ DSA/Stack/04-queue-implement-using-stack-amortized.cpp
@@ -0,0 +1,68 @@
+//--------- Raghav  Patidar---------
+// Fuck Ratings,Just Enjoy the Contest/Questions 
+// I'm in Love with Experience and Learning.
+// Prob : https://leetcode.com/problems/implement-queue-using-stacks/
+// Day : 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class MyQueue {
+public:
+    // IMPLEMENTATION USING TWO STACKS: all operations would be performed on the output stack
+    /*
+        Amortized O(1) push and pop operations
+    */
+    /** Initialize your data structure here. */
+    stack<int> input, output;
+    MyQueue() {
+
+    }
+
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        input.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int e; // top element that needs to be returned
+        // move elements from input to output
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        e = output.top();
+        output.pop();
+        return e;
+    }
+
+    /** Get the front element. */
+    int peek() {
+        // move elements from input to output
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        return output.top();
+    }
+
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
