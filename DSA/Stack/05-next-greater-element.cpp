d88286ad70537d640f54ce636497f8b65808a693
raghavpatidar <raghavpatidar997764@gmail.com>
2023-05-27 20:53:42 +0530

    NGR

---
+++ DSA/Stack/05-next-greater-element.cpp
@@ -0,0 +1,29 @@
+//--------- Raghav  Patidar---------
+// Fuck Ratings,Just Enjoy the Contest/Questions 
+// I'm in Love with Experience and Learning.
+// Prob :  https://leetcode.com/problems/next-greater-element-ii/
+// Day : 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> result(n); // to store the result
+        stack<int> s; // to store the indices of the next greater number.
+        // we are not storing numbers because of duplicates
+
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            while (!s.empty() and arr[s.top()] <= arr[i % n]) {
+                s.pop();
+            }
+            result[i % n] = (s.empty() ? -1 : arr[s.top()]);
+            // push the current index on the stack
+            s.push(i % n);
+        }
+        return result;
+    }
+};
