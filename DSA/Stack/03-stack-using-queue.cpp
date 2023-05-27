b56753c152ed58514358d49ffb3d40a47780da0a
raghavpatidar <raghavpatidar997764@gmail.com>
2023-05-27 20:53:09 +0530

    stack-using-queue

---
+++ DSA/Stack/03-stack-using-queue.cpp
@@ -0,0 +1,56 @@
+//--------- Raghav  Patidar---------
+// Fuck Ratings,Just Enjoy the Contest/Questions 
+// I'm in Love with Experience and Learning.
+// Prob :  https://leetcode.com/problems/implement-stack-using-queues/solution/
+// Day : 
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+class MyStack {
+public:
+    // IMPLEMENTATION USINIG A SINGLE QUEUE
+    /** Initialize your data structure here. */
+    queue<int> q;
+    MyStack() {
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        q.push(x);
+        // pop size - 1 elements and push them at the back
+        // this ensures the current element added t the end of the queue comes to the front
+        int size = q.size();
+        for (int i = 0; i < size - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int front = q.front();
+        q.pop();
+        return front;
+    }
+
+    /** Get the top element. */
+    int top() {
+        return q.front();
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
