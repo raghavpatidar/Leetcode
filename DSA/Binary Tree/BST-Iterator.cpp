82ea88a991c002d75f9f490655ea0d527b8a3d6b
raghavpatidar <raghavpatidar997764@gmail.com>
2023-06-05 13:46:51 +0530

    BST iterator.cpp

---
+++ DSA/Binary Tree/BST-Iterator.cpp
@@ -0,0 +1,62 @@
+/*
+┌────────────────────────────────────────────────────────────────────────────────────────┐
+│                      *-----------Raghav Patidar----------*                             |
+|                   Fuck Ratings,Just Enjoy the Contest/Questions                        |
+|                      I'm in Love with Experience and Learning.                         |
+│                                                                                        |
+|    Leetcode   : https://leetcode.com/manito7/                                          |
+│    Github     : https://github.com/raghavpatidar                                       |
+|    LinkedIn   : https://www.linkedin.com/in/raghavpatidar07/                           |
+|    Resume     : https://drive.google.com/file/d/1nsTqWKSzxl-tcIXARi_b6vMSVcz57cVQ/view |
+|    Codeforces : https://codeforces.com/profile/raghavpatidar                           |
+|                                                                                        |
+└────────────────────────────────────────────────────────────────────────────────────────┘
+
+Problem : https://leetcode.com/problems/binary-search-tree-iterator/submissions/958927441/
+Day :
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//  * Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class BSTIterator {
+public:
+public:
+    stack<TreeNode*> st;
+    BSTIterator(TreeNode* root) {
+        pushAll(root);
+    }
+    void pushAll(TreeNode* node) {
+        while (node) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+    int next() {
+        TreeNode* curr = st.top();
+        st.pop();
+        pushAll(curr->right);
+        return curr->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
