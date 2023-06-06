2cfdf599c6be7485f8b6b0c7b2b643258fd1d132
raghavpatidar <raghavpatidar997764@gmail.com>
2023-06-06 13:17:38 +0530

    Day 395

---
+++ Streak/june2022/06-check-if-given-array-is-AP.cpp
@@ -0,0 +1,32 @@
+/*
+┌────────────────────────────────────────────────────────────────────────────────────────┐
+│                      *-----------Raghav Patidar----------*                             |
+|                   Fuck Ratings,Just Enjoy the Contest/Questions                        |
+|                      I'm in Love with Experience and Learning.                         |
+│                                                                                        |
+|    Leetcode   : https://leetcode.com/manito7/                                          |
+│    Github     : https://github.com/raghavpatidar                                       |
+|    LinkedIn   : https://www.linkedin.com/in/raghavpatidar07/                           |
+|    Resume     : https://drive.google.com/file/d/1nsTqWKSzxl-tcIXARi_b6vMSVcz57cVQ/view |
+|    Codeforces : https://codeforces.com/profile/raghavpatidar                           |
+|                                                                                        |
+└────────────────────────────────────────────────────────────────────────────────────────┘
+
+Problem : https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/description/
+Day : 395
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        int d = arr[1] - arr[0];
+        for (int i = 1; i < arr.size();i++) {
+            if (arr[i] - arr[i - 1] != d)return false;
+        }
+        return true;
+    }
+};
