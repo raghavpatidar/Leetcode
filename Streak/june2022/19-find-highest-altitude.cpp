5a8cbf6e2590da63a2b9dadcd1d23eaa166b3cec
raghavpatidar <raghavpatidar997764@gmail.com>
2023-06-19 20:59:03 +0530

    day 408

---
+++ Streak/june2022/19-find-highest-altitude.cpp
@@ -0,0 +1,33 @@
+/*
+┌────────────────────────────────────────────────────────────────────────────────────────┐
+│                      *-----------Raghav Patidar----------*                             |
+|                   Fuck Ratings,Just Enjoy the Contest/Questions                        |
+|                      I'm in Love with Experience and Learning.                         |
+│                                                                                        |
+|    Leetcode   : https://leetcode.com/manito7/                                          |
+|    CodingNinja: https://bit.ly/3IW2Nqi                                                 |
+│    Github     : https://github.com/raghavpatidar                                       |
+|    LinkedIn   : https://www.linkedin.com/in/raghavpatidar07/                           |
+|    Resume     : https://drive.google.com/file/d/1nsTqWKSzxl-tcIXARi_b6vMSVcz57cVQ/view |
+|    Codeforces : https://codeforces.com/profile/raghavpatidar                           |
+|                                                                                        |
+└────────────────────────────────────────────────────────────────────────────────────────┘
+
+Problem : https://leetcode.com/problems/find-the-highest-altitude/
+Day : 408
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int pref = 0, ans = 0;
+        for (auto i : gain) {
+            pref += i;
+            ans = max(ans, pref);
+        }
+        return ans;
+    }
+};
