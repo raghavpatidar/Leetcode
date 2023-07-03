811019adfde9b58fcddd6545bf79c0c456edd882
raghavpatidar <raghavpatidar997764@gmail.com>
2023-07-03 22:18:22 +0530

    day 422

---
+++ Streak/july2023/3-buddy-strings.cpp
@@ -0,0 +1,33 @@
+/*
+┌────────────────────────────────────────────────────────────────────────────────────────┐
+│                      *-----------Raghav Patidar----------*                             |
+|                   Fuck Ratings,Just Enjoy the Contest/Questions                        |
+|                      I'm in Love with Experience and Learning.                         |
+│                                                                                        |
+|    Leetcode   : https://leetcode.com/manito7/                                          |
+|    CodingNinja: https://bit.ly/3IW2Nqi                                                 |
+│    Github     : https://github.com/raghavpatidar                                       |
+|    LinkedIn   : https://www.linkedin.com/in/raghavpatidar07/                           |
+|    Resume     : https://drive.google.com/file/d/1nsTqWKSzxl-tcIXARi_b6vMSVcz57cVQ/view |
+|    Codeforces : https://codeforces.com/profile/raghavpatidar                           |
+|                                                                                        |
+└────────────────────────────────────────────────────────────────────────────────────────┘
+
+Problem :https://leetcode.com/problems/buddy-strings/submissions/985469453/s
+Day : 422
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool buddyStrings(string A, string B) {
+        if (A.length() != B.length()) return false;
+        if (A == B && set<char>(A.begin(), A.end()).size() < A.size()) return true;
+        vector<int> dif;
+        for (int i = 0; i < A.length(); ++i) if (A[i] != B[i]) dif.push_back(i);
+        return dif.size() == 2 && A[dif[0]] == B[dif[1]] && A[dif[1]] == B[dif[0]];
+    }
+};
